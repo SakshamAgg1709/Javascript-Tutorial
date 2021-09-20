87ba7db87796db16f1f14523c1dbd217caebc94c
Saksham <sabusiness1709@gmail.com>
2021-09-20 14:48:07 +0530

    Functions of Console

---
+++ tut2.js
@@ -0,0 +1,17 @@
+// You should enter semicolon after every line 
+console.time('Your code took:')//It is for finding the time taken by the code
+console.log("Hello Console");
+console.log(44+4);
+console.log(44);
+console.log(true);
+console.log([2,33,45,11]);
+console.log({harry:'this', saksham:'that'} )
+console.table({harry:'this', saksham:'that'} )
+console.warn("This is a warning")
+console.error("This is a error")
+// console.clear()
+console.timeEnd('Your code took:')// It will tell how much time does the code took till this line (timeEnd)
+let age = 330
+console.assert(age>189 , "This is not possible")// It will give this statement if age is less than 189;
+// Agar left wali condition TRUE h to thik h nhi to right side wali statement print kar do
+// IT WILL PRINT - Assertion Failed
