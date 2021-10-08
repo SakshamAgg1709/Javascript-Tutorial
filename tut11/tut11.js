d2b26ceae4ecb675751193351f7fd53cf7dc68fd
Saksham <sabusiness1709@gmail.com>
2021-10-08 14:26:16 +0530

    Manipulating Website Using Client-Side Javascript

---
+++ tut11/tut11.js
@@ -0,0 +1,28 @@
+console.log("This is Tut11")
+let a = window.document;// Window Object is global in Client Side Javascript
+//Window ke andar Javascript ke saare methods hote h '
+
+// Learn More ABOUT WINDOW HERE - https://www.w3schools.com/js/js_window.asp
+
+// window.alert("Hi")
+// alert("Hi")  - Same effect as window.alert("Hi")
+
+// a = prompt("This will destroy your computer, Type your name")// Whatever we write in Prompt is saved in the variable a and is printed in console
+// console.log(a)
+
+// a = confirm("Are you sure you want to open this page")
+// console.log(a);
+
+// Nowadays we don't use alert, confirm or prompt - we use CSS and other Javascript Methods-
+
+a = window.innerHeight
+a = innerHeight
+a = window.innerWidth
+a = scrollX// How much you have scrolles horizontally
+a = scrollY// How much you have scrolled vertically
+a = location// It iss the location of the page - server, hosting, domain etc.
+a = location.toString()// It will give domain name of the page in the form of string
+
+a = window.history;// It tells you all the websites you have visited in that particular tab
+// To know more about HISTORY - https://developer.mozilla.org/en-US/docs/Web/API/History_API
+console.log(a)
